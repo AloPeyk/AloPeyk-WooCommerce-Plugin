617cd417e51398d2d7da55c8d6c18af7c12f9e01
Bijan Moudi <moudi@dpm.co.com>
2018-01-31 12:01:17 +0330

    Adding vendors

---
+++ vendor/alopeyk/alopeyk-api-php/src/AloPeykApiHandler.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace AloPeyk;
+
+use AloPeyk\Config\Configs;
+use AloPeyk\Exception\AloPeykApiException;
+use AloPeyk\Validator\AloPeykValidator;
+
+class AloPeykApiHandler
+{
+    private static $localToken;
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
+    {
+        if (method_exists($this, $name)) {
+            return self::$name($arguments);
+        }
+        throw new AloPeykApiException('AloPeyk API: This Function Does Not Exist!');
+    }
+
+    /**
+     * @param string $endPoint
+     * @param string $method
+     * @param null $postFields
+     * @return array
+     * @throws AloPeykApiException
+     */
+    private static function getCurlOptions($endPoint = '', $method = 'GET', $postFields = null)
+    {
+        /*
+         * Throw Exception If User Machine DOES NOT ABLE To Use 'openssl'
+         */
+        if (!extension_loaded('openssl')) {
+            throw new AloPeykApiException('AloPeyk API Needs The Open SSL PHP Extension! please enable it on your server.');
+        }
+        /*
+         * Get ACCESS-TOKEN
+         */
+        $accessToken = self::getToken();
+        if (!$accessToken) {
+            throw new AloPeykApiException('Invalid ACCESS-TOKEN! 
+            All AloPeyk API endpoints support the JWT authentication protocol. 
+            To start sending authenticated HTTP requests you will need to use your JWT authorization token which is sent to you.
+            Put it in: vendor/alopeyk/alopeyk-api-php/src/Config/Configs.php : TOKEN const 
+            ');
+        }
+        $curlOptions = [
+            CURLOPT_URL => Configs::API_URL . $endPoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_HTTPHEADER => [
+                'Authorization: Bearer ' . $accessToken,
+                'Content-Type: application/json; charset=utf-8',
+                'X-Requested-With: XMLHttpRequest'
+            ],
+        ];
+        if ($method == 'GET') {
+            $curlOptions[CURLOPT_CUSTOMREQUEST] = 'GET';
+        } else {
+            $curlOptions[CURLOPT_CUSTOMREQUEST] = 'POST';
+            $curlOptions[CURLOPT_POSTFIELDS] = json_encode($postFields);
+        }
+        return $curlOptions;
+    }
+
+    /**
+     * @param $curlObject
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    private static function getApiResponse($curlObject)
+    {
+        $response = curl_exec($curlObject);
+        $err = curl_error($curlObject);
+        curl_close($curlObject);
+        if ($err) {
+            throw new AloPeykApiException($err);
+        } else {
+            return json_decode($response);
+        }
+    }
+
+    /**
+     * @param $localToken
+     */
+    public static function setToken($localToken)
+    {
+        self::$localToken = $localToken;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getToken()
+    {
+        $accessToken = empty(self::$localToken) ? Configs::TOKEN : self::$localToken;
+        if (empty($accessToken) || $accessToken == "PUT-YOUR-ACCESS-TOKEN-HERE") {
+            return false;
+        }
+        return $accessToken;
+    }
+
+    /** ----------------------------------------------------------------------------------------------------------------
+     * public functions
+     * ---------------------------------------------------------------------------------------------------------------- */
+
+    /**
+     * Authentication
+     * @param  $withConfig
+     */
+    public static function authenticate($withConfig = false)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions("?withconfig={$withConfig}"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $latitude
+     * @param $longitude
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    public static function getAddress($latitude, $longitude)
+    {
+        $curl = curl_init();
+
+
+        if (!AloPeykValidator::validateLatitude($latitude)) {
+            throw new AloPeykApiException('Latitude is not correct');
+        }
+        if (!AloPeykValidator::validateLongitude($longitude)) {
+            throw new AloPeykApiException('Longitude is not correct');
+        }
+
+
+        curl_setopt_array($curl, self::getCurlOptions("locations?latlng=$latitude,$longitude"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $locationName
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    public static function getLocationSuggestion($locationName)
+    {
+        $curl = curl_init();
+
+        $locationName = AloPeykValidator::sanitize($locationName);
+        if (empty($locationName)) {
+            throw new AloPeykApiException('Location Name can not be empty!');
+        }
+
+        curl_setopt_array($curl, self::getCurlOptions("locations?input=$locationName"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $order
+     * @return mixed
+     */
+    public static function getPrice($order)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions('orders/price/calc', 'POST', $order->toArray('getPrice')));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $order
+     * @return mixed
+     */
+    public static function createOrder($order)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions('orders', 'POST', $order->toArray('createOrder')));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $orderID
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    public static function getOrderDetail($orderID)
+    {
+        $curl = curl_init();
+
+        $orderID = AloPeykValidator::sanitize($orderID);
+        if (!filter_var($orderID, FILTER_VALIDATE_INT)) {
+            throw new AloPeykApiException('OrderID must be integer!');
+        }
+
+        curl_setopt_array($curl, self::getCurlOptions("orders/{$orderID}?columns=*,addresses,screenshot,progress,courier_info,next_address_any,customer,last_position_minimal,eta_minimal"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $orderID
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    public static function cancelOrder($orderID, $comment)
+    {
+        $curl = curl_init();
+
+        $orderID = AloPeykValidator::sanitize($orderID);
+        if (!filter_var($orderID, FILTER_VALIDATE_INT)) {
+            throw new AloPeykApiException('OrderID must be integer!');
+        }
+
+        curl_setopt_array($curl, self::getCurlOptions("orders/{$orderID}/cancel?comment={$comment}"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * @param $orderID
+     * @param  $params parameters of comment and rate which are passed inside the $params array
+     * @return mixed
+     * @throws AloPeykApiException
+     */
+    public static function finishOrder($orderID, $params)
+    {
+        $curl = curl_init();
+
+        $orderID = AloPeykValidator::sanitize($orderID);
+        if (!filter_var($orderID, FILTER_VALIDATE_INT)) {
+            throw new AloPeykApiException('OrderID must be integer!');
+        }
+
+        curl_setopt_array($curl, self::getCurlOptions("orders/{$orderID}/finish", "POST", $params));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * User Profile
+     * @return  mixed
+     */
+    public static function getUserProfile()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions("show-profile?columns=*,credit"));
+
+        return self::getApiResponse($curl);
+    }    
+
+    /**
+     * Coupon Validation
+     * @return  mixed
+     */
+    public static function validateCoupon($code)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions("coupons", "POST", $code));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * Payment Route List
+     * @return array
+     */
+    public static function getPaymentGateways()
+    {
+        return array_keys(Configs::PAYMENT_ROUTES);
+    }
+
+    /**
+     * Credit Top-Up
+     * @param $user_id
+     * @param $amount
+     * @return string
+     */
+    public static function getPaymentRoute($user_id, $amount, $gateway = 'saman')
+    {
+        return Configs::API_URL . Configs::PAYMENT_ROUTES[$gateway] . "?user_id={$user_id}&amount={$amount}";
+    }
+
+    /**
+     * Retrieve tracking url for the order
+     * @param $orderToken
+     * @return string
+     */
+    public static function getTrackingUrl($orderToken)
+    {
+        return Configs::TRACKING_URL . "#/{$orderToken}";
+    }
+
+    /**
+     * Retrieve the print invoice for the order
+     * @param $orderId
+     * @param $orderToken
+     * @return string
+     */
+    public static function getPrintInvoice($orderId, $orderToken)
+    {
+        return Configs::URL . "order/{$orderId}/print?token={$orderToken}";
+    }
+
+    /**
+     * Retrieve the application configuration array
+     * @return mixed
+     */
+    public static function getAppConfig()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, self::getCurlOptions("config"));
+
+        return self::getApiResponse($curl);
+    }
+
+    /**
+     * Retrieve the full path of the uploaded signature
+     * @return string
+     */
+    public static function getSignaturePath($relativePath)
+    {
+        return Configs::URL . $relativePath;
+    }
+}
---
+++ vendor/alopeyk/alopeyk-api-php/src/Model/Order.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace AloPeyk\Model;
+
+use AloPeyk\AloPeykApiHandler;
+use AloPeyk\Config\Configs;
+use AloPeyk\Exception\AloPeykApiException;
+use AloPeyk\Validator\AloPeykValidator;
+
+class Order
+{
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    private $transportType;
+    private $city;
+    private $originAddress;
+    private $destinationsAddress;
+    private $hasReturn;
+    private $cashed;
+    private $scheduled_at;
+
+    public function __construct($transportType, $originAddress, $destinationsAddress, $scheduled_at = null)
+    {
+        $this->setTransportType($transportType);
+        $this->addOriginAddress($originAddress);
+        $this->setHasReturn(false);
+        $this->setCashed(false);
+
+        if($scheduled_at)
+        {
+            $this->setScheduledAt($scheduled_at);
+        }
+
+        $this->destinationsAddress = [];
+        if (!is_array($destinationsAddress)) {
+            $this->addDestinationsAddress($destinationsAddress);
+        } else {
+            foreach ($destinationsAddress as $destAddress) {
+                $this->addDestinationsAddress($destAddress);
+            }
+        }
+    }
+
+    // Setters ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @param $transportType
+     * @throws AloPeykApiException
+     */
+    public function setTransportType($transportType)
+    {
+        $transportType = AloPeykValidator::sanitize($transportType);
+        if (!in_array($transportType, array_values(Configs::TRANSPORT_TYPES))) {
+            throw new AloPeykApiException('Transport Type is not correct');
+        }
+
+        $this->transportType = $transportType;
+    }
+
+    /**
+     * Set scheduled_at attribute
+     * @param $scheduledAt
+     */
+    public function setScheduledAt($scheduled_at)
+    {
+        $this->scheduled_at = $scheduled_at;
+    }
+
+    /**
+     * @param $originAddress
+     * @throws AloPeykApiException
+     */
+    public function addOriginAddress($originAddress)
+    {
+        if (!$originAddress instanceof Address) {
+            throw new AloPeykApiException('Origin Address is not valid!');
+        }
+
+        if ($originAddress->getType() != 'origin') {
+            throw new AloPeykApiException('Type Of Origin Address is not correct! please change it to `origin`.');
+        }
+
+        $this->originAddress = $originAddress;
+        $this->city = $originAddress->getCity();
+    }
+
+    /**
+     * @param $newDestinationsAddress
+     * @throws AloPeykApiException
+     */
+    public function addDestinationsAddress($newDestinationsAddress)
+    {
+        if (!$newDestinationsAddress instanceof Address) {
+            throw new AloPeykApiException('Destination Address is not valid!');
+        }
+
+        if ($newDestinationsAddress->getType() != 'destination') {
+            throw new AloPeykApiException('Type Of Destination Address is not correct! please change it to `destination`.');
+        }
+
+        array_push($this->destinationsAddress, $newDestinationsAddress);
+    }
+
+    /**
+     * @param mixed $hasReturn
+     */
+    public function setHasReturn($hasReturn)
+    {
+        $this->hasReturn = $hasReturn;
+    }
+
+    /**
+     * @param mixed $cashed
+     */
+    public function setCashed($cashed)
+    {
+        $this->cashed = $cashed;
+    }
+
+    // Getters ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return mixed
+     */
+    public function getTransportType()
+    {
+        return $this->transportType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginAddress()
+    {
+        return $this->originAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestinationsAddress()
+    {
+        return $this->destinationsAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestinationsAddressArray()
+    {
+        $addresses = [];
+        foreach ($this->getDestinationsAddress() as $address) {
+            array_push($addresses, $address->toArray('destination'));
+        }
+        return $addresses;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHasReturn()
+    {
+        return $this->hasReturn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCashed()
+    {
+        return $this->cashed;
+    }
+
+    // Actions ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return mixed
+     */
+    public function create()
+    {
+        return AloPeykApiHandler::createOrder($this);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return AloPeykApiHandler::getPrice($this);
+    }
+
+    public function getScheduledAt()
+    {
+        return $this->scheduled_at;
+    }
+
+    /**
+     * @param $orderID
+     * @return mixed
+     */
+    public static function cancel($orderID, $comment)
+    {
+        return AloPeykApiHandler::cancelOrder($orderID, $comment);
+    }
+
+    /**
+     * @param $orderID
+     * @return mixed
+     */
+    public static function finish($orderID, $params)
+    {
+        return AloPeykApiHandler::finishOrder($orderID, $params);
+    }
+
+    /**
+     * @param $orderID
+     * @return mixed
+     */
+    public static function getDetails($orderID)
+    {
+        return AloPeykApiHandler::getOrderDetail($orderID);
+    }
+
+    // Utilities -------------------------------------------------------------------------------------------------------
+
+    /**
+     * @param $endPoint
+     * @return array
+     */
+    public function toArray($endPoint)
+    {
+        $this->isValid();
+
+        $orderArray = [
+            'city' => $this->city,
+            'transport_type' => $this->getTransportType(),
+            'has_return' => $this->getHasReturn(),
+            'cashed' => $this->getCashed(),
+            'scheduled_at' => $this->getScheduledAt()
+        ];
+
+        $orderArray['addresses'] = array_merge(
+            [$this->getOriginAddress()->toArray($endPoint)],
+            $this->getDestinationsAddressArray()
+        );
+
+        return $orderArray;
+    }
+
+    /**
+     * @return bool
+     * @throws AloPeykApiException
+     */
+    private function isValid()
+    {
+        // CHECK CITY
+        if (AloPeykValidator::sanitize($this->city) != $this->getOriginAddress()->getCity()) {
+            throw new AloPeykApiException('Origin Address is not valid!');
+        }
+
+        // CHECK TRANSPORT_TYPE
+        if (!in_array($this->getTransportType(), array_values(Configs::TRANSPORT_TYPES))) {
+            throw new AloPeykApiException('Transport Type is not correct!');
+        }
+
+        // CHECK ORIGIN
+        if (!$this->getOriginAddress()) {
+            throw new AloPeykApiException('Each Order Requires One Origin Address!');
+        }
+        if ($this->getOriginAddress()->getType() != 'origin') {
+            throw new AloPeykApiException('Type Of Origin Address is not correct! please change it to `origin`.');
+        }
+
+        // CHECK DESTINATIONS
+        if (count($this->getDestinationsAddress()) < 1) {
+            throw new AloPeykApiException('Each Order Requires At Least One Destination!');
+        }
+        foreach ($this->getDestinationsAddress() as $destination) {
+            if ($destination->getType() != 'destination') {
+                throw new AloPeykApiException('Type of Destination Address is not correct! please change it to `destination`.');
+            }
+        }
+
+        return true;
+    }
+
+}
